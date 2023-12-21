229afd374ca401a008cc6199b25633f15a13fad6
Marcos Pereira <vinicius.hein@gmail.com>
2023-12-21 01:48:47 -0300

    feat: cria rotas privadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Login } from './pages/Login';
+import { PrivateRoutes } from './routes/PrivateRoute';
 import './App.css';
 
 function App() {
-  const router = createBrowserRouter([{ path: '/', element: <Login /> }]);
+  const router = createBrowserRouter([
+    { path: '/', element: <PrivateRoutes component={() => <div>Books</div>} /> },
+    { path: '/login', element: <Login /> },
+    { path: '*', element: <PrivateRoutes component={() => <div>Books</div>} /> },
+  ]);
 
   return <RouterProvider router={router} />;
 }
---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { RootState } from '../redux/store';
+
+type PrivateRoutesProps = {
+  component: React.ComponentType;
+};
+
+export function PrivateRoutes({ component: RouteComponent }: PrivateRoutesProps) {
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+  if (isAuthenticated) {
+    return <RouteComponent />;
+  }
+
+  return <Navigate to="/login" />;
+}
